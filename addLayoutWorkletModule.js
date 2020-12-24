bbaf4c156b8bcbd42a0b943e442be0aa6556fd16
Matt Spatola <mattjspatola@gmail.com>
2020-12-24 18:15:03 -0500

    initial commit

---
+++ addLayoutWorkletModule.js
@@ -0,0 +1,7 @@
+export default async function addLayoutWorkletModule(url) {
+  if ( globalThis?.CSS?.layoutWorklet ) {
+    return await CSS.layoutWorklet.addModule(url);
+  } else {
+    throw new Error("globalThis.CSS.layoutWorklet does not exist!");
+  )
+}
